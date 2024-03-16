e50152957974cfb97f8b65688a52e129f2dff836
Stephen-Montague <stephen.w.montague@gmail.com>
2024-03-17 07:30:45 +0900

    Begin adding front-end support for back-end web worker.

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+
 if (window.Worker) {
     const worker = new Worker("worker.js", {type: "module"});
-    console.log("Worker object created.");
+    console.log("Worker created.");
+
+    // Listener to receive worker messages.
+    onMessage = function(event) 
+    {
+        // event.data has whatever was passed from the worker.
+        // We can update the front-end from this data here.
+
+    }
+
+    // Example of how to send a message to the worker.
+    // The argument can be null, a single item, or a single array, if sending more than one item.
+    // Note that only copies of native values or data object literals can be sent. 
+    // So a class instance sent (not recommended) would be converted to a generic data object, losing type & methods.
+    // Likely, we'll want to send messages to change setup values and to trigger starting.
+    worker.postMessage();
 }
 else
 {
